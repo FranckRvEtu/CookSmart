f7af086177964b76924d04788bc87af0efba1b4a
FranckRvEtu <franck.reveille@etu.umontpellier.fr>
2025-05-27 16:04:33 +0200

    Mise en page correcte

--- app/src/main/java/com/example/projet/MainActivity.kt
+++ app/src/main/java/com/example/projet/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.projet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.projet.theme.MonTheme
 import com.example.projet.ui.login.LoginScreen
@@ -11,9 +13,13 @@ class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?){
 
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent{
             MonTheme {
-                AppNavigation()
+                Surface {
+                    AppNavigation()
+                }
             }
         }
     }
